d2cea7b716bb053defa806c38d0f7973ac8ab6ac
PratikAwaik <pratikawaik125@gmail.com>
2021-05-18 20:49:10 +0530

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = "mongodb+srv://pratik:pratikdb@cluster0.hjurk.mongodb.net/local_library?retryWrites=true&w=majority";
+const dev_db_url = "mongodb+srv://pratik:pratikdb@cluster0.hjurk.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error: "));
